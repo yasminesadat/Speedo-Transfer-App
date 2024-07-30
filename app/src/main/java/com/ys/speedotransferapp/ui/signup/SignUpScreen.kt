7e92cd3f5223b6201049b193b14895c2bac49667
Mohamed-Ahmed-Esmat <21p0144@eng.asu.edu.eg>
2024-07-30 15:02:28 +0300

    added ui files

---
+++ app/src/main/java/com/ys/speedotransferapp/ui/signup/SignUpScreen.kt
@@ -0,0 +1,10 @@
+package com.ys.speedotransferapp.ui.signup
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun SignUpScreen(modifier: Modifier = Modifier) {
+
+}
+
