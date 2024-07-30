4ab20af77ac90b402a8e8e9e5923e1c7c476c751
Mohamed-Ahmed-Esmat <21p0144@eng.asu.edu.eg>
2024-07-30 19:50:13 +0300

    signIn and main page signUp done

--- app/src/main/java/com/ys/speedotransferapp/ui/signup/SignUpViewModel.kt
+++ app/src/main/java/com/ys/speedotransferapp/ui/signup/SignUpViewModel.kt
@@ -1,4 +1,36 @@
 package com.ys.speedotransferapp.ui.signup
 
-class SignUpViewModel {
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
+
+class SignUpViewModel: ViewModel() {
+    private val _fullName = MutableStateFlow("")
+    val fullName = _fullName.asStateFlow()
+
+    private val _email = MutableStateFlow("")
+    val email = _email.asStateFlow()
+
+    private val _password = MutableStateFlow("")
+    val password = _password.asStateFlow()
+
+
+    fun setFullName(fullName: String) {
+            _fullName.value = fullName
+    }
+
+    fun setEmail(email: String) {
+        _email.value = email
+    }
+
+    fun setPassword(password: String) {
+        _password.value = password
+    }
+
+
+
+
 }
