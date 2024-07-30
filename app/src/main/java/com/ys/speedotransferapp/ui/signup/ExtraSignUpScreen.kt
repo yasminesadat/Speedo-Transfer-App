9e627953699f87f83b0bc01edce8307a89ebe430
Mohamed-Ahmed-Esmat <21p0144@eng.asu.edu.eg>
2024-07-30 20:06:21 +0300

    added a new screen

---
+++ app/src/main/java/com/ys/speedotransferapp/ui/signup/ExtraSignUpScreen.kt
@@ -0,0 +1,9 @@
+package com.ys.speedotransferapp.ui.signup
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun ExtraSignUpScreen(modifier: Modifier = Modifier) {
+    
+}
