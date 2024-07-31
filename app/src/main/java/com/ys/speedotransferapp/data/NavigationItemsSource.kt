3b7747833e66ebff621c298904697f0f8e1a909b
Yasmine Elsadat <yasmine.atef2004@gmail.com>
2024-07-31 16:44:44 +0300

    uncommentedChanges

--- app/src/main/java/com/ys/speedotransferapp/data/NavigationItemsSource.kt
+++ app/src/main/java/com/ys/speedotransferapp/data/NavigationItemsSource.kt
@@ -1,59 +1,59 @@
-//package com.ys.speedotransferapp.data
-//
-//import com.ys.speedotransferapp.R
-//import com.ys.speedotransferapp.model.BottomNavigationItem
-//import com.ys.speedotransferapp.navigation.AppRoutes.CARDS_ROUTE
-//import com.ys.speedotransferapp.navigation.AppRoutes.HOME_ROUTE
-//import com.ys.speedotransferapp.navigation.AppRoutes.MORE_ROUTE
-//import com.ys.speedotransferapp.navigation.AppRoutes.TRANSACTIONS_ROUTE
-//import com.ys.speedotransferapp.navigation.AppRoutes.TRANSFER_ROUTE
-//
-//class NavigationItemsSource {
-//    fun get(): List<BottomNavigationItem> {
-//        val destinations = mutableListOf<BottomNavigationItem>().apply {
-//            add(
-//                BottomNavigationItem(
-//                    route = HOME_ROUTE,
-//                    selectedIcon = R.drawable.home_selected,
-//                    unselectedIcon = R.drawable.home,
-//                    iconText = "Home"
-//                )
-//            )
-//            add(
-//                BottomNavigationItem(
-//                    route = TRANSFER_ROUTE,
-//                    selectedIcon = R.drawable.transfer_selected,
-//                    unselectedIcon = R.drawable.transfer,
-//                    iconText = "Transfer"
-//                )
-//            )
-//            add(
-//                BottomNavigationItem(
-//                    route = TRANSACTIONS_ROUTE,
-//                    selectedIcon = R.drawable.transactions_selected,
-//                    unselectedIcon = R.drawable.transactions,
-//                    iconText = "Transactions"
-//                )
-//            )
-//            add(
-//                BottomNavigationItem(
-//                    route = CARDS_ROUTE,
-//                    selectedIcon = R.drawable.mycards_selected,
-//                    unselectedIcon = R.drawable.mycards,
-//                    iconText = "My cards"
-//                )
-//            )
-//            add(
-//                BottomNavigationItem(
-//                    route = MORE_ROUTE,
-//                    selectedIcon = R.drawable.more_selected,
-//                    unselectedIcon = R.drawable.more,
-//                    iconText = "More"
-//                )
-//            )
-//
-//        }
-//
-//        return destinations
-//    }
-//}
+package com.ys.speedotransferapp.data
+
+import com.ys.speedotransferapp.R
+import com.ys.speedotransferapp.model.BottomNavigationItem
+import com.ys.speedotransferapp.navigation.AppRoutes.CARDS_ROUTE
+import com.ys.speedotransferapp.navigation.AppRoutes.HOME_ROUTE
+import com.ys.speedotransferapp.navigation.AppRoutes.MORE_ROUTE
+import com.ys.speedotransferapp.navigation.AppRoutes.TRANSACTIONS_ROUTE
+import com.ys.speedotransferapp.navigation.AppRoutes.TRANSFER_ROUTE
+
+class NavigationItemsSource {
+    fun get(): List<BottomNavigationItem> {
+        val destinations = mutableListOf<BottomNavigationItem>().apply {
+            add(
+                BottomNavigationItem(
+                    route = HOME_ROUTE,
+                    selectedIcon = R.drawable.home_selected,
+                    unselectedIcon = R.drawable.home,
+                    iconText = "Home"
+                )
+            )
+            add(
+                BottomNavigationItem(
+                    route = TRANSFER_ROUTE,
+                    selectedIcon = R.drawable.transfer_selected,
+                    unselectedIcon = R.drawable.transfer,
+                    iconText = "Transfer"
+                )
+            )
+            add(
+                BottomNavigationItem(
+                    route = TRANSACTIONS_ROUTE,
+                    selectedIcon = R.drawable.transactions_selected,
+                    unselectedIcon = R.drawable.transactions,
+                    iconText = "Transactions"
+                )
+            )
+            add(
+                BottomNavigationItem(
+                    route = CARDS_ROUTE,
+                    selectedIcon = R.drawable.mycards_selected,
+                    unselectedIcon = R.drawable.mycards,
+                    iconText = "My cards"
+                )
+            )
+            add(
+                BottomNavigationItem(
+                    route = MORE_ROUTE,
+                    selectedIcon = R.drawable.more_selected,
+                    unselectedIcon = R.drawable.more,
+                    iconText = "More"
+                )
+            )
+
+        }
+
+        return destinations
+    }
+}
--- app/src/main/java/com/ys/speedotransferapp/ui/more/MoreScreen.kt
+++ app/src/main/java/com/ys/speedotransferapp/ui/more/MoreScreen.kt
@@ -1,133 +1,133 @@
-//package com.ys.speedotransferapp.ui.more
-//
-//import androidx.annotation.DrawableRes
-//import androidx.compose.foundation.background
-//import androidx.compose.foundation.layout.Box
-//import androidx.compose.foundation.layout.Column
-//import androidx.compose.foundation.layout.Row
-//import androidx.compose.foundation.layout.fillMaxSize
-//import androidx.compose.foundation.layout.fillMaxWidth
-//import androidx.compose.foundation.layout.padding
-//import androidx.compose.foundation.layout.size
-//import androidx.compose.material3.HorizontalDivider
-//import androidx.compose.material3.Icon
-//import androidx.compose.material3.MaterialTheme
-//import androidx.compose.material3.Text
-//import androidx.compose.runtime.Composable
-//import androidx.compose.ui.Alignment
-//import androidx.compose.ui.Modifier
-//import androidx.compose.ui.graphics.Brush
-//import androidx.compose.ui.graphics.Color
-//import androidx.compose.ui.res.painterResource
-//import androidx.compose.ui.tooling.preview.Preview
-//import androidx.compose.ui.unit.dp
-//import androidx.navigation.NavController
-//import androidx.navigation.compose.rememberNavController
-//import com.ys.speedotransferapp.R
-//import com.ys.speedotransferapp.data.OptionsSource
-//import com.ys.speedotransferapp.ui.theme.CosmicLatte
-//import com.ys.speedotransferapp.ui.theme.LightRose
-//
-//@Composable
-//fun MoreScreen(
-//    navController: NavController
-//) {
-//    Box(
-//        modifier = Modifier.background(
-//            brush = Brush.verticalGradient(
-//                colors = listOf(CosmicLatte, LightRose)
-//            )
-//        )
-//    ) {
-//        Column(
-//            modifier = Modifier
-//                .fillMaxSize()
-//                .padding(horizontal = 8.dp)
-//                .padding(top = 48.dp)
-//
-//        ) {
-//            Header()
-//            val options = OptionsSource().getOptions()
-//            for (option in options) {
-//                Option(
-//                    icon = option.icon,
-//                    title = option.title,
-//                    isLast = option.isLast
-//                )
-//            }
-//        }
-//
-//    }
-//}
-//
-//@Composable
-//fun Header() {
-//    Box(
-//        modifier = Modifier
-//            .fillMaxWidth()
-//            .padding(bottom = 48.dp)
-//    ) {
-//        Icon(
-//            painter = painterResource(id = R.drawable.back_chevron),
-//            contentDescription = null,
-//            modifier = Modifier
-//                .size(16.dp)
-//                .align(Alignment.CenterStart),
-//            tint = Color.Unspecified
-//        )
-//        Text(
-//            text = "More",
-//            style = MaterialTheme.typography.headlineMedium,
-//            modifier = Modifier.align(Alignment.Center)
-//        )
-//    }
-//}
-//
-//
-//@Composable
-//fun Option(
-//    @DrawableRes icon: Int,
-//    title: String,
-//    isLast: Boolean = false
-//) {
-//    Column {
-//        Row(
-//            verticalAlignment = Alignment.CenterVertically,
-//            modifier = Modifier.padding(8.dp)
-//        ) {
-//            Icon(
-//                painter = painterResource(id = icon),
-//                contentDescription = null,
-//                modifier = Modifier
-//                    .padding(end = 8.dp)
-//                    .size(20.dp),
-//                tint = Color.Unspecified
-//            )
-//
-//            Text(
-//                text = title,
-//                style = MaterialTheme.typography.titleMedium,
-//                modifier = Modifier.weight(1f)
-//            )
-//
-//            if (!isLast) {
-//                Icon(
-//                    painter = painterResource(id = R.drawable.chevron),
-//                    contentDescription = null,
-//                    modifier = Modifier.size(24.dp),
-//                    tint = Color.Unspecified
-//                )
-//            }
-//        }
-//
-//        if (!isLast) {
-//            HorizontalDivider(Modifier.padding(all = 8.dp))
-//        }
-//    }
-//}
-//
-//@Preview(showBackground = true)
-//@Composable
-//private fun MoreScreenPreview() {
-//    MoreScreen(rememberNavController())
-//}
+package com.ys.speedotransferapp.ui.more
+
+import androidx.annotation.DrawableRes
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
+import androidx.navigation.compose.rememberNavController
+import com.ys.speedotransferapp.R
+import com.ys.speedotransferapp.data.OptionsSource
+import com.ys.speedotransferapp.ui.theme.CosmicLatte
+import com.ys.speedotransferapp.ui.theme.LightRose
+
+@Composable
+fun MoreScreen(
+    navController: NavController
+) {
+    Box(
+        modifier = Modifier.background(
+            brush = Brush.verticalGradient(
+                colors = listOf(CosmicLatte, LightRose)
+            )
+        )
+    ) {
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(horizontal = 8.dp)
+                .padding(top = 48.dp)
+
+        ) {
+            Header()
+            val options = OptionsSource().getOptions()
+            for (option in options) {
+                Option(
+                    icon = option.icon,
+                    title = option.title,
+                    isLast = option.isLast
+                )
+            }
+        }
+
+    }
+}
+
+@Composable
+fun Header() {
+    Box(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(bottom = 48.dp)
+    ) {
+        Icon(
+            painter = painterResource(id = R.drawable.back_chevron),
+            contentDescription = null,
+            modifier = Modifier
+                .size(16.dp)
+                .align(Alignment.CenterStart),
+            tint = Color.Unspecified
+        )
+        Text(
+            text = "More",
+            style = MaterialTheme.typography.headlineMedium,
+            modifier = Modifier.align(Alignment.Center)
+        )
+    }
+}
+
+
+@Composable
+fun Option(
+    @DrawableRes icon: Int,
+    title: String,
+    isLast: Boolean = false
+) {
+    Column {
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = Modifier.padding(8.dp)
+        ) {
+            Icon(
+                painter = painterResource(id = icon),
+                contentDescription = null,
+                modifier = Modifier
+                    .padding(end = 8.dp)
+                    .size(20.dp),
+                tint = Color.Unspecified
+            )
+
+            Text(
+                text = title,
+                style = MaterialTheme.typography.titleMedium,
+                modifier = Modifier.weight(1f)
+            )
+
+            if (!isLast) {
+                Icon(
+                    painter = painterResource(id = R.drawable.chevron),
+                    contentDescription = null,
+                    modifier = Modifier.size(24.dp),
+                    tint = Color.Unspecified
+                )
+            }
+        }
+
+        if (!isLast) {
+            HorizontalDivider(Modifier.padding(all = 8.dp))
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun MoreScreenPreview() {
+    MoreScreen(rememberNavController())
+}
