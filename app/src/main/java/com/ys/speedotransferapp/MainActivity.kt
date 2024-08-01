f9ed4135113574a56f737a6af653a94a8f09c004
Yasmine Elsadat <yasmine.atef2004@gmail.com>
2024-08-01 17:05:14 +0300

    AdjustedColorNames

--- app/src/main/java/com/ys/speedotransferapp/MainActivity.kt
+++ app/src/main/java/com/ys/speedotransferapp/MainActivity.kt
@@ -4,15 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.Scaffold
-import androidx.compose.ui.Modifier
 import com.ys.speedotransferapp.navigation.MainScreen
-import com.ys.speedotransferapp.ui.favourite.FavouriteScreen
-import com.ys.speedotransferapp.ui.landing.LandingPageScreen
-import com.ys.speedotransferapp.ui.signup.ExtraSignUpScreen
-import com.ys.speedotransferapp.ui.signup.SignUpScreen
-import com.ys.speedotransferapp.ui.signup.SignUpViewModel
 import com.ys.speedotransferapp.ui.theme.SpeedoTransferAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -21,11 +13,7 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             SpeedoTransferAppTheme {
-                Scaffold(
-                    modifier = Modifier.fillMaxSize()
-                ) { _ ->
                     MainScreen()
-                }
             }
         }
     }
