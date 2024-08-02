d90f3087b04d7bdef0871c934aaa59bf20d8237d
Yasmine Elsadat <yasmine.atef2004@gmail.com>
2024-08-02 03:05:06 +0300

    viewModelAdjustmentsToUserInput

--- app/src/main/java/com/ys/speedotransferapp/ui/home/HomeScreen.kt
+++ app/src/main/java/com/ys/speedotransferapp/ui/home/HomeScreen.kt
@@ -1,20 +1,53 @@
 package com.ys.speedotransferapp.ui.home
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.GenericShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.geometry.toRect
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 @Composable
-fun HomeScreen() {
+fun HomeScreen(
+    viewModel: HomeViewModel = HomeViewModel()
+) {
     Column(
-        modifier = Modifier
-            .padding(16.dp)
-            .fillMaxSize()
+        modifier = Modifier.fillMaxSize()
     ) {
-        Text("HOME")
+     Row(){
+      //   CircularText(viewModel.getInitials())
+     }
+    }
+}
+
+@Composable
+fun CircularText(text: String) {
+    Box(
+        modifier = Modifier
+            .size(52.dp)
+            .clip(GenericShape { size, _ ->
+                addOval(size.toRect())
+            })
+            .background(Color.Gray),
+        contentAlignment = Alignment.Center
+    ) {
+        Text(
+            text = text,
+            color = Color.White,
+            fontWeight = FontWeight.Bold,
+            fontSize = 16.sp
+        )
     }
 }
