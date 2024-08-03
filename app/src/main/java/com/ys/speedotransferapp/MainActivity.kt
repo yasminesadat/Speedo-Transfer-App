de135b0b4330e0f67459c0577c0fe43fc421723a
Yasmine Elsadat <yasmine.atef2004@gmail.com>
2024-08-03 17:03:18 +0300

    formattingChange

--- app/src/main/java/com/ys/speedotransferapp/MainActivity.kt
+++ app/src/main/java/com/ys/speedotransferapp/MainActivity.kt
@@ -5,14 +5,16 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import com.ys.speedotransferapp.ui.navigation.MainScreen
+import com.ys.speedotransferapp.ui.theme.SpeedoTransferAppTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            MainScreen()
+            SpeedoTransferAppTheme {
+                MainScreen()
+            }
         }
     }
 }
-
