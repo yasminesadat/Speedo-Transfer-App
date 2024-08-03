1e5a0cdcf604be5cbb47f0f3a143e6965c1d9db4
Yasmine Elsadat <yasmine.atef2004@gmail.com>
2024-08-03 16:59:17 +0300

    addedTransacDetailsScreen

--- app/src/main/java/com/ys/speedotransferapp/MainActivity.kt
+++ app/src/main/java/com/ys/speedotransferapp/MainActivity.kt
@@ -1,7 +1,5 @@
 package com.ys.speedotransferapp
 
-import android.annotation.SuppressLint
-import android.content.pm.ActivityInfo
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
