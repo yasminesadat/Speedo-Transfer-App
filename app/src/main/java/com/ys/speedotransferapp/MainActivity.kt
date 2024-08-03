6f49e9fb0d3e32bc9f15103841dfdcd52c35eaba
Yasmine Elsadat <yasmine.atef2004@gmail.com>
2024-08-03 16:13:38 +0300

    orientationModification

--- app/src/main/java/com/ys/speedotransferapp/MainActivity.kt
+++ app/src/main/java/com/ys/speedotransferapp/MainActivity.kt
@@ -1,21 +1,21 @@
 package com.ys.speedotransferapp
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import com.ys.speedotransferapp.ui.navigation.MainScreen
-import com.ys.speedotransferapp.ui.theme.SpeedoTransferAppTheme
 
 class MainActivity : ComponentActivity() {
+    @SuppressLint("SourceLockedOrientationActivity")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            SpeedoTransferAppTheme {
                     MainScreen()
-            }
         }
+        //requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_PORTRAIT
     }
 }
 
